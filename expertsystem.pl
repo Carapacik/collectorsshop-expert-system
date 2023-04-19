75178f1e079f481d9b8871e205a72f1392966891
Roman Laptev <carapacik@gmail.com>
2023-04-19 16:00:22 +0300

    General group search fixes

--- expertsystem.pl
+++ expertsystem.pl
@@ -35,51 +35,62 @@ item("Dark Behemoth", 1499, [0,4,9,11,13]).
 item("The strings of Suradan", 2699, [0,5,7,12,14]).
 item("Blue Horizons", 999, [0,4,9,11,13]).
 item("Crimson Dawn", 249, [0,4,9,11,13]).
-item("Годовая подписка Dota+", 3050, [3,6,10,14]).
+item("Year Dota+", 3050, [3,6,9,10,14]).
 
 :- dynamic item/3.
 
-
-prompt(Answer, Answers, AnswersNew, Q) :-
-  member(Answer, ['y','yes']),
-  append(Answers, [Q], AnswersNew), !.
-prompt(_, AnswersNew, AnswersNew, _).
-
 ask_to_add_item(Answers) :-
-    nl, write("Не удалось найти скин с указанными свойствами. Хотите добавить новый предмет с этими свойствами? (y/n)"), nl,
-    read(Answer),
-    (member(Answer, ['y', 'yes']) ->
-        write("Введите название предмета: "), nl,
-        read(ItemName),
-        write("Введите цену предмета: "), nl,
-        read(ItemPrice),
-        assertz(item(ItemName, ItemPrice, Answers)),
-        write("Новый предмет добавлен."), nl, nl
-    ;
-        write("Не добавлено."), nl, nl
-    ).
+  nl, write("Не удалось найти скин с указанными свойствами. Хотите добавить новый предмет с этими свойствами? (y/n)"), nl,
+  read(Answer),
+  (member(Answer, ['y', 'yes']) ->
+    write("Введите название предмета: "), nl,
+    read(ItemName),
+    write("Введите цену предмета: "), nl,
+    read(ItemPrice),
+    assertz(item(ItemName, ItemPrice, Answers)),
+    write("Новый предмет добавлен."), nl, nl
+  ;
+    write("Не добавлено."), nl, nl
+  ).
 
 check(Answers, _) :-
-    item(X, Y, Answers), nl,
-    write("Скин найден, это - "),  write(X), nl,
-    write("Цена предмета - "), write(Y), nl, nl,
-    main.
+  item(X, Y, Answers), nl,
+  write("Скин найден, это - "),  write(X), nl,
+  write("Цена предмета - "), write(Y), nl,
+  write("Свойства - "), write(Answers), nl, nl,
+  main.
 check(Answers, Q) :-
-    property(Q, _),
-    Q1 is Q + 1,
-    run(Q1, Answers), !.
+  property(Q, _),
+  Q1 is Q + 1,
+  run(Q1, Answers), !.
 check(Answers, _) :-
-    \+ item(_, _, Answers),
-    ask_to_add_item(Answers), main, !.
+  \+ item(_, _, Answers),
+  ask_to_add_item(Answers), main, !.
+
+last_in_same_group(Q) :-
+  property_group(Group),
+  member(Q, Group),
+  Q1 is Q + 1,
+  not(member(Q1, Group)).
 
 belongs_to_same_group(Q1, Q2) :-
-    property_group(Group),
-    member(Q1, Group),
-    member(Q2, Group).
+  property_group(Group),
+  member(Q1, Group),
+  member(Q2, Group).
 
 answered_in_same_group(Answers, Q) :-
-    member(Answer, Answers),
-    belongs_to_same_group(Q, Answer).
+  member(Answer, Answers),
+  belongs_to_same_group(Q, Answer).
+
+prompt(Answer, Answers, AnswersNew, Q) :-
+  (member(Answer, ['y','yes']) ->
+    append(Answers, [Q], AnswersNew), !
+  ;
+    Q1 is Q + 1,
+    last_in_same_group(Q1),
+    append(Answers, [Q1], AnswersNew)
+  ).
+prompt(_, AnswersNew, AnswersNew, _).
 
 run(Q, Answers) :-
   property(Q, Question),
@@ -93,9 +104,9 @@ run(Q, Answers) :-
   ).
 
 process(1):-
-    run(0, []).
+  run(0, []).
 process(_) :-
-    write("Выход из программы").
+  write("Выход из программы").
 
 main :-
   write("1 - Начать поиск"), nl,
